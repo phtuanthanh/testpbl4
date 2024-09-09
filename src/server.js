3a5da706e690ef43e4a4679e9719862c2ca11972
phtuanthanh <thanhloveliv2004@gmail.com>
2024-09-09 21:22:59 +0700

    v1

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", "./src/view");
+
+app.get('/account', function (req, res) {
+  return res.render('account.ejs')
+})
+app.get('/baithi', function (req, res) {
+  return res.render('baithi.ejs')
+})
+app.get('/ketqua', function (req, res) {
+  return res.render('ketqua.ejs')
+})
+app.get('/thanhvien', function (req, res) {
+  return res.render('thanhvien.ejs')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
